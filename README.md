837cb30cecc12d316670af85e052c585000e337d
Amrindra <amrindraouk@yahoo.com>
2022-02-01 20:23:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-lwp9ay
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-lwp9ay)
---
+++ models/Product.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    maxlength: 60,
+  },
+
+  desc: {
+    type: String,
+    required: true,
+    maxlength: 200,
+  },
+});
