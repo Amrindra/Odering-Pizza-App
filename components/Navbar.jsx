aff8aabd0a81114357c55043797ea515f6843f68
Amrindra <amrindraouk@yahoo.com>
2022-02-04 18:32:09 -0500

    worked on Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,29 @@
+import Image from 'next/image';
+import styles from '../styles/Navbar.module.css';
+
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <Image
+            src="https://img.icons8.com/cute-clipart/100/000000/ringing-phone.png"
+            alt=""
+            width="32"
+            height="32"
+          />
+        </div>
+
+        <div className={styles.nav_exts}>
+          <div className={styles.nav_text}>ORDER NOW</div>
+          <div className={styles.nav_text}>999 999 999</div>
+        </div>
+      </div>
+
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
 };
 
 export default Navbar;
