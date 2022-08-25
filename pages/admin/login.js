6792ae86e0506a9bc0210697aff0207211ac70bb
Amrindra Ouk <70451928+Amrindra@users.noreply.github.com>
2022-08-25 16:31:21 -0400

    Changed URL

--- pages/admin/login.js
+++ pages/admin/login.js
@@ -11,7 +11,7 @@ const Login = () => {
 
   const handleClick = async () => {
     try {
-      await axios.post("https://tonbb.sse.codesandbox.io/api/login", {
+      await axios.post("http://localhost:8000/api/login", {
         username,
         password
       });
